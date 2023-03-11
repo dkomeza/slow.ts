306baa3ed390cfb35305532aceda445cb0109bd8
Dawid Komęza <dawid.komeza@gmail.com>
2023-03-11 22:46:56 +0000

    Added basic routing support

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,4 +2,11 @@ import { app } from "../index.js";
 
 // const app = new slow();
 
-// app.listen();
+app.route("get", "/", (req, res) => {
+  // console.log(req);
+  res.end("Hello World");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running");
+});
