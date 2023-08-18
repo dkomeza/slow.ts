b8c5beb60ee9ce276bd02fcc82fe5d001ccd51c7
Dawid Komęza <dawid.komeza@gmail.com>
2023-08-18 09:30:39 +0000

    :sparkles: Add middleware

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
-/// <reference types="node" /> 
+/// <reference types="node" />
 
 import slow, { app } from "./build/slow";
+import SlowRequest from "./build/router/Request";
+import SlowResponse from "./build/router/Response";
 
 export default slow;
 export { app };
+export type { SlowRequest, SlowResponse };
